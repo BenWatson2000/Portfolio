5993719edb3fb29e38de59baeccd7a34465599d8
BenWatson2000 <benthompsonwatson@hotmail.co.uk>
2023-01-26 11:03:24 +0000

    deletion

--- old code/script.js
+++
@@ -1,9 +0,0 @@
-// let apples = 2
-// let bannanas = 3
-
-
-// for (let index = 0; index < 10; index++) {
-//     const newPara = document.createElement('p')
-//     newPara.textContent = 'This is a new paragraph!'
-//     document.body.prepend(newPara);
-// }
